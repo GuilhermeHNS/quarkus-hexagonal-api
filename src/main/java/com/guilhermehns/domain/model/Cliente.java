780c7ec3ed89d2e2c2d569cc94664219de8e5c71
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2026-03-19 09:08:34 -0300

    Criado entidade Cliente

---
+++ src/main/java/com/guilhermehns/domain/model/Cliente.java
@@ -0,0 +1,115 @@
+package com.guilhermehns.domain.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Cliente {
+
+    private String id;
+    private String nomeCompleto;
+    private String nomeMae;
+    private String cpf;
+    private String rg;
+    private String email;
+    private String telefone;
+    private LocalDate dataNascimento;
+    private LocalDateTime dataCadastro;
+
+    private Endereco endereco;
+
+    public Cliente() {
+    }
+
+    public Cliente(String id, String nomeCompleto, String nomeMae, String cpf, String rg, String email, String telefone, LocalDate dataNascimento, LocalDateTime dataCadastro, Endereco endereco) {
+        this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.nomeMae = nomeMae;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.email = email;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.dataCadastro = dataCadastro;
+        this.endereco = endereco;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
