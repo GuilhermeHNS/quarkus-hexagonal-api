8a4b69edcc1adedeb288ba031017ee95861b8d6d
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2026-03-20 10:29:58 -0300

    Criado validações para persistencia de cliente

--- src/main/java/com/guilhermehns/application/usecase/cliente/CriarClienteUseCase.java
+++ src/main/java/com/guilhermehns/application/usecase/cliente/CriarClienteUseCase.java
@@ -1,6 +1,7 @@
 package com.guilhermehns.application.usecase.cliente;
 
 import com.guilhermehns.domain.model.cliente.Cliente;
+import com.guilhermehns.domain.model.cliente.Endereco;
 import com.guilhermehns.domain.repository.ClienteRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -17,17 +18,47 @@ public class CriarClienteUseCase {
     }
 
     public Cliente executar(Cliente cliente) {
-        if (cliente.getNomeCompleto() == null || cliente.getNomeCompleto().isBlank()) {
-            throw new RuntimeException("Nome é obrigatório");
-        }
-
-        if (cliente.getCpf() == null || cliente.getCpf().isBlank()) {
-            throw new RuntimeException("CPF é obrigatório");
-        }
-
+        validarCliente(cliente);
         cliente.setId(UUID.randomUUID());
         cliente.setDataCadastro(LocalDateTime.now());
-
         return repository.save(cliente);
+    }
+
+    private void validarCliente(Cliente cliente) {
+        if (cliente == null) {
+            throw new IllegalArgumentException("Cliente é obrigatório");
+        }
+
+        validarCampoObrigatorio(cliente.getNomeCompleto(), "Nome completo é obrigatório");
+        validarCampoObrigatorio(cliente.getNomeMae(), "Nome da mãe é obrigatório");
+        validarCampoObrigatorio(cliente.getCpf(), "CPF é obrigatório");
+        validarCampoObrigatorio(cliente.getRg(), "RG é obrigatório");
+        validarCampoObrigatorio(cliente.getEmail(), "Email é obrigatório");
+        validarCampoObrigatorio(cliente.getTelefone(), "Telefone é obrigatório");
+
+        if (cliente.getDataNascimento() == null) {
+            throw new IllegalArgumentException("Data de nascimento é obrigatória");
+        }
+
+        validarEndereco(cliente.getEndereco());
+    }
+
+    private void validarCampoObrigatorio(String valor, String mensagem) {
+        if (valor == null || valor.isBlank()) {
+            throw new IllegalArgumentException(mensagem);
+        }
+    }
+
+    private void validarEndereco(Endereco endereco) {
+        if (endereco == null) {
+            throw new IllegalArgumentException("Endereço é obrigatório");
+        }
+
+        validarCampoObrigatorio(endereco.getCep(), "CEP é obrigatório");
+        validarCampoObrigatorio(endereco.getLogradouro(), "Logradouro é obrigatório");
+        validarCampoObrigatorio(endereco.getNumero(), "Número é obrigatório");
+        validarCampoObrigatorio(endereco.getBairro(), "Bairro é obrigatório");
+        validarCampoObrigatorio(endereco.getCidade(), "Cidade é obrigatória");
+        validarCampoObrigatorio(endereco.getEstado(), "Estado é obrigatório");
     }
 }
--- src/test/java/com/guilhermehns/application/usecase/cliente/CriarClienteUseCaseTest.java
+++ src/test/java/com/guilhermehns/application/usecase/cliente/CriarClienteUseCaseTest.java
@@ -1,25 +1,33 @@
 package com.guilhermehns.application.usecase.cliente;
 
 import com.guilhermehns.domain.model.cliente.Cliente;
+import com.guilhermehns.domain.model.cliente.Endereco;
 import com.guilhermehns.domain.repository.ClienteRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 public class CriarClienteUseCaseTest {
 
-    @Test
-    void deveCriarClienteComSucesso(){
-        ClienteRepository repository =  Mockito.mock(ClienteRepository.class);
+    private ClienteRepository repository;
+    private CriarClienteUseCase useCase;
 
-        CriarClienteUseCase useCase = new CriarClienteUseCase(repository);
-        Cliente cliente = new Cliente();
-        cliente.setNomeCompleto("João Silva");
-        cliente.setCpf("12345678900");
-        cliente.setDataNascimento(LocalDate.of(1990, 1, 1));
+    @BeforeEach
+    public void mockaDependencias() {
+        repository = Mockito.mock(ClienteRepository.class);
+        useCase = new CriarClienteUseCase(repository);
+    }
+
+    @Test
+    void deveCriarClienteComSucesso() {
+        Cliente cliente = criarClienteValido();
 
         Mockito.when(repository.save(Mockito.any()))
                 .thenAnswer(invocation -> invocation.getArgument(0));
@@ -29,36 +37,324 @@ public class CriarClienteUseCaseTest {
         assertNotNull(resultado.getId());
         assertNotNull(resultado.getDataCadastro());
         assertEquals("João Silva", resultado.getNomeCompleto());
+        Mockito.verify(repository, Mockito.times(1)).save(cliente);
+        Mockito.verifyNoMoreInteractions(repository);
 
     }
 
     @Test
-    void deveLancarErroQuandoNomeForNulo() {
-        ClienteRepository repository = Mockito.mock(ClienteRepository.class);
-        CriarClienteUseCase useCase = new CriarClienteUseCase(repository);
+    void deveLancarExcecaoQuandoClienteForNulo() {
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(null)
+        );
 
-        Cliente cliente = new Cliente();
+        assertEquals("Cliente é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoNomeCompletoForNulo() {
+        Cliente cliente = criarClienteValido();
         cliente.setNomeCompleto(null);
-        cliente.setCpf("12345678900");
-        cliente.setDataNascimento(LocalDate.of(1990,1,1));
 
-        assertThrows(RuntimeException.class, () -> {
-            useCase.executar(cliente);
-        });
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Nome completo é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
     }
 
     @Test
-    void deveLancarErroQuandoCpfForNulo() {
-        ClienteRepository repository = Mockito.mock(ClienteRepository.class);
-        CriarClienteUseCase useCase = new CriarClienteUseCase(repository);
+    void deveLancarExcecaoQuandoNomeCompletoForVazio() {
 
+        Cliente cliente = criarClienteValido();
+        cliente.setNomeCompleto(" ");
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Nome completo é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoNomeMaeForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.setNomeMae(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Nome da mãe é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoCpfForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.setCpf(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("CPF é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoRgForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.setRg(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("RG é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoEmailForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.setEmail(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Email é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoTelefoneForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.setTelefone(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Telefone é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoDataNascimentoForNula() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.setDataNascimento(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Data de nascimento é obrigatória", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+
+    @Test
+    void deveLancarExcecaoQuandoEnderecoForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.setEndereco(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Endereço é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoCepForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.getEndereco().setCep(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("CEP é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoLogradouroForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.getEndereco().setLogradouro(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Logradouro é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoNumeroForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.getEndereco().setNumero(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Número é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoBairroForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.getEndereco().setBairro(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Bairro é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoCidadeForNula() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.getEndereco().setCidade(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Cidade é obrigatória", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoEstadoForNulo() {
+
+        Cliente cliente = criarClienteValido();
+        cliente.getEndereco().setEstado(null);
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Estado é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoCpfForVazio() {
+        Cliente cliente = criarClienteValido();
+        cliente.setCpf(" ");
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("CPF é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoEmailForVazio() {
+        Cliente cliente = criarClienteValido();
+        cliente.setEmail(" ");
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Email é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoTelefoneForVazio() {
+        Cliente cliente = criarClienteValido();
+        cliente.setTelefone(" ");
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("Telefone é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoCepForVazio() {
+        Cliente cliente = criarClienteValido();
+        cliente.getEndereco().setCep(" ");
+
+        IllegalArgumentException excecao = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.executar(cliente)
+        );
+
+        assertEquals("CEP é obrigatório", excecao.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    private Cliente criarClienteValido() {
         Cliente cliente = new Cliente();
         cliente.setNomeCompleto("João Silva");
-        cliente.setCpf(null);
-        cliente.setDataNascimento(LocalDate.of(1990, 1, 1));
+        cliente.setNomeMae("Maria Souza");
+        cliente.setCpf("12345678900");
+        cliente.setRg("123456789");
+        cliente.setEmail("guilherme@email.com");
+        cliente.setTelefone("19999999999");
+        cliente.setDataNascimento(LocalDate.of(2000, 1, 1));
 
-        assertThrows(RuntimeException.class, () -> {
-            useCase.executar(cliente);
-        });
+        Endereco endereco = new Endereco();
+        endereco.setCep("13400-000");
+        endereco.setLogradouro("Rua A");
+        endereco.setNumero("123");
+        endereco.setComplemento("Casa");
+        endereco.setBairro("Centro");
+        endereco.setCidade("Limeira");
+        endereco.setEstado("SP");
+
+        cliente.setEndereco(endereco);
+
+        return cliente;
     }
 }
