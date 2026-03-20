5d86541746cdd3727f5ceb58ed6be0fbcbfb1e31
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2026-03-20 07:54:12 -0300

    Corrigido test de relatorio de maior faturamento

--- src/test/java/com/guilhermehns/application/usecase/relatorio/RelatorioMaiorFaturamentoUseCaseTest.java
+++ src/test/java/com/guilhermehns/application/usecase/relatorio/RelatorioMaiorFaturamentoUseCaseTest.java
@@ -16,56 +16,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RelatorioMaiorFaturamentoUseCaseTest {
     @Test
-    void deveRetornarOs4ItensDeMaiorFaturamento() {
+    void deveRetornarProdutosComMaiorFaturamento() {
         VendaRepository vendaRepository = Mockito.mock(VendaRepository.class);
-
         RelatorioMaiorFaturamentoUseCase useCase = new RelatorioMaiorFaturamentoUseCase(vendaRepository);
 
-        Produto produto1 = new Produto();
-        produto1.setId(UUID.randomUUID());
-        produto1.setNome("Produto 1");
-        produto1.setPrecoVenda(new BigDecimal("10.00"));
+        UUID produtoId1 = UUID.randomUUID();
+        UUID produtoId2 = UUID.randomUUID();
 
-        Produto produto2 = new Produto();
-        produto2.setId(UUID.randomUUID());
-        produto2.setNome("Produto 2");
-        produto2.setPrecoVenda(new BigDecimal("20.00"));
+        ItemMaiorFaturamentoDTO item1 = new ItemMaiorFaturamentoDTO();
+        item1.setProdutoId(produtoId1);
+        item1.setNomeProduto("Mouse Logitech Sem Fio");
+        item1.setPrecoVenda(new BigDecimal("89.90"));
+        item1.setFaturamento(new BigDecimal("42000.00"));
 
-        Produto produto3 = new Produto();
-        produto3.setId(UUID.randomUUID());
-        produto3.setNome("Produto 3");
-        produto3.setPrecoVenda(new BigDecimal("30.00"));
+        ItemMaiorFaturamentoDTO item2 = new ItemMaiorFaturamentoDTO();
+        item2.setProdutoId(produtoId2);
+        item2.setNomeProduto("Notebook Dell Inspiron");
+        item2.setPrecoVenda(new BigDecimal("3500.00"));
+        item2.setFaturamento(new BigDecimal("719.20"));
 
-        Produto produto4 = new Produto();
-        produto4.setId(UUID.randomUUID());
-        produto4.setNome("Produto 4");
-        produto4.setPrecoVenda(new BigDecimal("40.00"));
+        List<ItemMaiorFaturamentoDTO> retornoEsperado = List.of(item1, item2);
 
-        Produto produto5 = new Produto();
-        produto5.setId(UUID.randomUUID());
-        produto5.setNome("Produto 5");
-        produto5.setPrecoVenda(new BigDecimal("50.00"));
-
-        ItemVenda item1 = new ItemVenda(produto1, 2, new BigDecimal("10.00")); // 20
-        ItemVenda item2 = new ItemVenda(produto2, 3, new BigDecimal("20.00")); // 60
-        ItemVenda item3 = new ItemVenda(produto3, 4, new BigDecimal("30.00")); // 120
-        ItemVenda item4 = new ItemVenda(produto4, 5, new BigDecimal("40.00")); // 200
-        ItemVenda item5 = new ItemVenda(produto5, 6, new BigDecimal("50.00")); // 300
-
-        Venda venda = new Venda();
-        venda.setItens(List.of(item1, item2, item3, item4, item5));
-
-        Mockito.when(vendaRepository.findAllVendas())
-                .thenReturn(List.of(venda));
+        Mockito.when(vendaRepository.buscarProdutosComMaiorFaturamento())
+                .thenReturn(retornoEsperado);
 
         List<ItemMaiorFaturamentoDTO> resultado = useCase.executar();
 
-        assertEquals(4, resultado.size());
-        assertEquals(produto5.getId(), resultado.get(0).getProdutoId());
-        assertEquals(produto4.getId(), resultado.get(1).getProdutoId());
-        assertEquals(produto3.getId(), resultado.get(2).getProdutoId());
-        assertEquals(produto2.getId(), resultado.get(3).getProdutoId());
+        assertEquals(2, resultado.size());
+        assertEquals(produtoId1, resultado.getFirst().getProdutoId());
+        assertEquals("Mouse Logitech Sem Fio", resultado.get(0).getNomeProduto());
+        assertEquals(new BigDecimal("42000.00"), resultado.get(0).getFaturamento());
 
-        Mockito.verify(vendaRepository).findAllVendas();
+        assertEquals(produtoId2, resultado.get(1).getProdutoId());
+        assertEquals("Notebook Dell Inspiron", resultado.get(1).getNomeProduto());
+        assertEquals(new BigDecimal("719.20"), resultado.get(1).getFaturamento());
+
+        Mockito.verify(vendaRepository).buscarProdutosComMaiorFaturamento();
     }
 }
